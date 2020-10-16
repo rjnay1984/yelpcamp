288d8e5284aac7dcf7317d39bb6882ac048c2f2a
RJ Nay <rjnay1984@gmail.com>
2020-10-16 15:39:15 -0500

    Create Campground.cs

---
+++ YelpcampRazorPages/YelpcampRazorPages/Models/Campground.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YelpcampRazorPages.Models
+{
+    public class Campground
+    {
+        public int CampgroundId { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string Teaser { get; set; }
+        public string Body { get; set; }
+    }
+}
