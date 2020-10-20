71b8ca60bc2da19def62af73cf6dc4589a6ad83e
RJ Nay <rjnay1984@gmail.com>
2020-10-20 14:45:17 -0500

    split commentlist into view component

---
+++ YelpcampRazorPages/YelpcampRazorPages/ViewComponents/CommentListViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YelpcampRazorPages.Models;
+
+namespace YelpcampRazorPages.Pages.Campgrounds.Comments
+{
+    public class CommentListViewComponent : ViewComponent
+    {
+        private readonly YelpcampRazorPages.Data.ApplicationDbContext _context;
+
+        public CommentListViewComponent(YelpcampRazorPages.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+            int campgroundId)
+        {
+            var comments = await GetCommentsAsync(campgroundId);
+            return View(comments);
+        }
+
+        private Task<List<Comment>> GetCommentsAsync(int campgroundId)
+        {
+            return _context.Comment
+                .Where(c => c.CampgroundId == campgroundId)
+                .ToListAsync();
+        }
+    }
+}
