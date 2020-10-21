ad323a2b18b5749a589e6de6c4a015014dcafa65
RJ Nay <rjnay1984@gmail.com>
2020-10-21 16:09:02 -0500

    Correcting namespaces

--- dotnet/YelpcampRazorPages/src/WebAdmin/Pages/Privacy.cshtml.cs
+++ dotnet/YelpcampRazorPages/src/WebAdmin/Pages/Privacy.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace WebAdmin.Pages
+namespace YelpcampRazorPages.WebAdmin.Pages
 {
     public class PrivacyModel : PageModel
     {
--- dotnet/YelpcampRazorPages/src/WebAdmin/Program.cs
+++ dotnet/YelpcampRazorPages/src/WebAdmin/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace WebAdmin
+namespace YelpcampRazorPages.WebAdmin
 {
     public class Program
     {
