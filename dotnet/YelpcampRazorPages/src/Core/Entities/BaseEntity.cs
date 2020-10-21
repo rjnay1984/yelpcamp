5853a4949574628888a253068218725ba5a536b1
RJ Nay <rjnay1984@gmail.com>
2020-10-21 16:05:35 -0500

    Create BaseEntity.cs

---
+++ dotnet/YelpcampRazorPages/src/Core/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YelpcampRazorPages.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        public virtual int ID { get; protected set; }
+    }
+}
